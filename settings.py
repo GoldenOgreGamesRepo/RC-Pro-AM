069b108a638403b3abc69f1e57dc9ca7decec6f3
GoldenOgreGamesRepo <damian.goldenogre@gmail.com>
2026-02-12 13:15:03 -0500

    Updated maps

--- settings.py
+++ settings.py
@@ -22,56 +22,56 @@ TRACKS = {
     "track_2": {
         "image": "track_2.png",
         "collision": "track_2_collision.png",
-        "start_pos": (840, 1817),
+        "start_pos": (1666, 1700),
         "start_angle": 0,
         "laps": 2,
         "checkpoints": [
-            (990, 1770), 
-            (1670, 1093), 
-            (1236, 442), 
-            (480, 747)
+            (1855, 1763), 
+            (2989, 1702), 
+            (2948, 600), 
+            (186, 927)
         ],
         "checkpoint_radius": 160  
     },
     "track_3": {
         "image": "track_3.png",
         "collision": "track_3_collision.png",
-        "start_pos": (840, 1817),
+        "start_pos": (1588, 65),
         "start_angle": 0,
-        "laps": 3,
+        "laps": 2,
         "checkpoints": [
-            (990, 1770), 
-            (1670, 1093), 
-            (1236, 442), 
-            (480, 747)
+            (1807, 238), 
+            (2983, 1549), 
+            (1678, 3170), 
+            (1425, 1817)
         ],
         "checkpoint_radius": 160  
     },
     "track_4": {
         "image": "track_4.png",
         "collision": "track_4_collision.png",
-        "start_pos": (840, 1817),
+        "start_pos": (1884, 234),
         "start_angle": 0,
-        "laps": 4,
+        "laps": 2,
         "checkpoints": [
-            (990, 1770), 
-            (1670, 1093), 
-            (1236, 442), 
-            (480, 747)
+            (2082, 236), 
+            (840, 1662), 
+            (2986, 3525), 
+            (180, 1689)
         ],
         "checkpoint_radius": 160  
     },
     "track_5": {
         "image": "track_5.png",
         "collision": "track_5_collision.png",
-        "start_pos": (840, 1817),
+        "start_pos": (2245, 1625),
         "start_angle": 0,
-        "laps": 5,
+        "laps": 2,
         "checkpoints": [
-            (990, 1770), 
-            (1670, 1093), 
-            (1236, 442), 
-            (480, 747)
+            (2438, 1632), 
+            (3265, 319), 
+            (155, 364), 
+            (3400, 2958)
         ],
         "checkpoint_radius": 160  
     },
